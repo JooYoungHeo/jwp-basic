ae637d7e44fb14b8afeb535dc230469aa8904128
JooYoungHeo <jyhur900226@naver.com>
2017-07-02 16:09:14 +0900

    [작업] Reflection - 두번째 요구사항 구현

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -2,10 +2,20 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] methods = clazz.getDeclaredMethods();
+        String str = "test";
 
+        for (Method method : methods){
+            String methodName = method.getName();
+            if(methodName.startsWith(str)){
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
